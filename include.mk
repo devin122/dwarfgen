cb19804765affaac980bee42d88636f92e575d4f
Devin Nakamura <devin122@gmail.com>
2023-08-28 14:25:53 -0400

    Add ability to create basic symbols

--- include.mk
+++ include.mk
@@ -1,5 +1,5 @@
 
-DWARFGEN_INCLUDES := $(patsubst %, $(DWARFGEN_DIR)/%, dwarf_consts.inc dwarf_macros.inc)
+DWARFGEN_INCLUDES := $(patsubst %, $(DWARFGEN_DIR)/%, dwarf_consts.inc dwarf_macros.inc symbols.inc)
 DWARFGEN_OBJS := $(patsubst %, $(OBJ_DIR)/core/%, base_types.o dwarf_common.o)
 
 $(DWARFGEN_OBJS): $(DWARFGEN_INCLUDES)
